64c8df60c54c295695df578c155188f1af700a26
shund <shundrikaleksey@gmail.com>
2017-09-15 23:28:47 +0300

    _write & _read release

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 //
 #include <stdio.h>
 #include <malloc.h>
+#include <mem.h>
 #include "mmemory.h"
 int main() {
     VA va1;
@@ -12,13 +13,13 @@ int main() {
     _malloc(&va1, 11);
     _malloc(&va2, 1);
     _malloc(&va3, 9);
-    printf("%d\n", va1);
-    printf("%d\n", va2);
-    printf("%d\n", va3);
+    char *buffer = "qwerty";
+    VA allocated_buffer = (char *) malloc(sizeof(char) * 6);
     _free(va1);
-    printf("%d\n", va1);
-    printf("%d\n", va2);
-    printf("%d\n", va3);
+    printf("%d\n", _write(va1, buffer, 10));
+    printf("%d\n", _read(va1+3, allocated_buffer, 9));
+//    printf("%s\n", allocated_buffer);
+
 
     return 0;
 }
--- mmemory.c
+++ mmemory.c
@@ -3,17 +3,18 @@
 //
 #include <malloc.h>
 #include <stdio.h>
+#include <mem.h>
 #include "mmemory.h"
 #include "memory_area.h"
 
 Linked_list *linked_list;
 
 int _malloc(VA* ptr, size_t szBlock) {
+    if (linked_list == NULL) { return UNKNOWN_ERROR; }
     int next_node_size;
     VA next_node_va;
     int index = 0;
     Node* index_node = linked_list->head;
-
     while (index_node != NULL) {
         if (index_node->value->isEmpty) {
             if (index_node->value->size == szBlock) {
@@ -37,21 +38,18 @@ int _malloc(VA* ptr, size_t szBlock) {
                 return SUCCESSFUL_IMPLEMENTATION;
             }
         }
-
-        if (index_node->next == NULL) {
-            return LACK_OF_MEMORY;
-        }
-
+        if (index_node->next == NULL) { return LACK_OF_MEMORY; }
         index++;
         index_node = index_node->next;
     }
     return UNKNOWN_ERROR;
 }
 int _init(int n, int szPage){
-    if (n  <  0 || szPage < 0) {
-        return INVALID_PARAMETERS;
-    }
+    if (n  <  0 || szPage < 0) { return INVALID_PARAMETERS; }
     linked_list = (Linked_list*)malloc(sizeof(Linked_list));
+    if (linked_list == NULL) {
+        return UNKNOWN_ERROR;
+    }
     int size = n * szPage;
     Node *new_node = init_node(size);
     linked_list->size = 1;
@@ -63,7 +61,7 @@ int _init(int n, int szPage){
 
 int _free(VA ptr) {
     Node *index_node = linked_list->head;
-    if(ptr==NULL) return -2;
+    if(ptr==NULL) return LACK_OF_MEMORY;
     while (index_node != NULL) {
         if (index_node->value->va == ptr && index_node->value->isEmpty == 0 ) {
             if (index_node->previous != NULL && index_node->previous->value->isEmpty) {
@@ -78,6 +76,40 @@ int _free(VA ptr) {
             }
             free(ptr); //не уверен на счет фри т.к. по идеи оно указывает на всю память и может удалить то что нам еще нужно
             return SUCCESSFUL_IMPLEMENTATION;
+        }
+        index_node = index_node->next;
+    }
+    return UNKNOWN_ERROR;
+}
+
+int _write(VA ptr, void* pBuffer, size_t szBuffer) {
+    if (ptr == NULL || pBuffer == NULL || szBuffer < 0) { return INVALID_PARAMETERS; }
+    if (linked_list == NULL) { return UNKNOWN_ERROR; }
+    Node *index_node = linked_list->head;
+    while (index_node != NULL) {
+        if (!index_node->value->isEmpty) {
+            if (index_node->value->va <= ptr && index_node->value->va + index_node->value->size > ptr) {
+                if (index_node->value->va + index_node->value->size - ptr < szBuffer) { return LACK_OF_MEMORY; }
+                memcpy(ptr, pBuffer, szBuffer);
+                return SUCCESSFUL_IMPLEMENTATION;
+            }
+        }
+        index_node = index_node->next;
+    }
+    return UNKNOWN_ERROR;
+}
+
+int _read(VA ptr, void* pBuffer, size_t szBuffer) {
+    if (ptr == NULL || pBuffer == NULL || szBuffer < 0) { return INVALID_PARAMETERS; }
+    if (linked_list == NULL) { return UNKNOWN_ERROR; }
+    Node *index_node = linked_list->head;
+    while (index_node != NULL) {
+        if (!index_node->value->isEmpty) {
+            if (index_node->value->va <= ptr && index_node->value->va + index_node->value->size > ptr) {
+                if (index_node->value->va + index_node->value->size - ptr < szBuffer) { return LACK_OF_MEMORY; }
+                memcpy(pBuffer, ptr, szBuffer);
+                return SUCCESSFUL_IMPLEMENTATION;
+            }
         }
         index_node = index_node->next;
     }
